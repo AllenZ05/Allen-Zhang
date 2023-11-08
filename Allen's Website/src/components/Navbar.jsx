1aac9cb4fe59f9fd2aebc1238e47775bc84d9ad5
Allen Zhang <allen.zhang.y05@gmail.com>
2023-11-08 18:20:23 -0500

    Created React Startup Code and NavBar

---
+++ Allen's Website/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import "./Navbar.css"
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className="header">
+        <Link to="/">
+            <h1>AZ</h1>
+        </Link>
+        <ul className="nav">
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+                <Link to="/contact">Contact</Link>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
