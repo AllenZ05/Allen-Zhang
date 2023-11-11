332dd02db4f8c7f3a9c0200ade7ec39de00b6860
Allen Zhang <allen.zhang.y05@gmail.com>
2023-11-11 14:42:57 -0500

    Making new component "more"

---
+++ Allen's Website/src/components/HeroMore.jsx
@@ -0,0 +1,14 @@
+import "./HeroMore.css";
+
+const HeroMore = () => {
+  return (
+    <div className="hero-more">
+      <div className="more-content">
+        <h2>More</h2>
+        <p>TODO</p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroMore;
