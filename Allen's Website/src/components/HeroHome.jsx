c89c480ac1954176b5c914cca1f6d36fc45b89f7
Allen Zhang <allen.zhang.y05@gmail.com>
2023-11-10 22:53:16 -0500

    Changing hero-content to hero-home to match naming

--- Allen's Website/src/components/HeroHome.jsx
+++ Allen's Website/src/components/HeroHome.jsx
@@ -7,7 +7,7 @@ const HeroHome = () => {
       <div className="images">
         <img className="background-img" src={HeroHomebg} alt="Background Image" />
       </div>
-      <div className="hero-content">
+      <div className="home-content">
         <p>Hi there, I am</p>
         <h1>Allen Zhang</h1>
       </div>
