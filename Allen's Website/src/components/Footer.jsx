a34baeff3fade4f3de034266ecb70d9efa8c29df
Allen Zhang <allen.zhang.y05@gmail.com>
2023-11-11 16:12:16 -0500

    Added react-toastify and improved Footer component

--- Allen's Website/src/components/Footer.jsx
+++ Allen's Website/src/components/Footer.jsx
@@ -3,24 +3,25 @@ import { AiOutlineCopyrightCircle } from "react-icons/ai";
 import { FaCanadianMapleLeaf } from "react-icons/fa6";
 
 import { MdEmail } from "react-icons/md";
-import { BsLinkedin } from "react-icons/bs";
+import { AiFillLinkedin } from "react-icons/ai";
 import { AiFillInstagram } from "react-icons/ai";
 import { AiOutlineGithub } from "react-icons/ai";
 import { BsDiscord } from "react-icons/bs";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const Footer = () => {
-  const copyToClipboard = async (text) => {
+  const copyToClipboard = async (type, text) => {
     try {
       await navigator.clipboard.writeText(text);
-      displayNotification(`Copied: ${text}`); 
+      toast.success(`Copied ${type} to clipboard!`, { style: { width: "20rem", fontSize: "0.85rem" } });
     } catch (err) {
-      console.error("Could not copy text: ", err);
-      displayNotification("Failed to copy text"); 
+      console.error(`Could not copy ${type}: `, err);
+      toast.error(`Failed to copy ${type}`);
     }
   };
-  const displayNotification = (message) => {
-    alert(message);
-  };
+
   return (
     <div className="footer">
       <div className="sections">
@@ -40,23 +41,23 @@ const Footer = () => {
         <div className="middle">
           <div className="socials">
             {/* Email */}
-            <button onClick={() => copyToClipboard("a367zhan@uwaterloo.ca")} className="email">
+            <button onClick={() => copyToClipboard("Email Address", "a367zhan@uwaterloo.ca")} className="email">
               <MdEmail className="icon email" />
             </button>
             {/* LinkedIn */}
             <a href="https://www.linkedin.com/in/allenzhang-05-/" target="_blank" rel="noopener noreferrer">
-              <BsLinkedin className="icon linkedIn" />
+              <AiFillLinkedin className="icon linkedIn" />
             </a>
             {/* Instagram */}
             <a href="https://www.instagram.com/allenz05/" target="_blank" rel="noopener noreferrer">
               <AiFillInstagram className="icon instagram" />
             </a>
             {/* GitHub */}
-            <a href="https://github.com/AllenZ05" target="_blank" rel="noopener noreferrer" >
-              <AiOutlineGithub className="icon github"/>
+            <a href="https://github.com/AllenZ05" target="_blank" rel="noopener noreferrer">
+              <AiOutlineGithub className="icon github" />
             </a>
             {/* Discord */}
-            <button onClick={() => copyToClipboard("az_05")} className="discord">
+            <button onClick={() => copyToClipboard("Discord Username", "az_05")} className="discord">
               <BsDiscord className="icon discord" />
             </button>
           </div>
@@ -70,6 +71,20 @@ const Footer = () => {
           </div>
         </div>
       </div>
+
+      {/* Toast Container */}
+      <ToastContainer
+        position="top-center"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </div>
   );
 };
